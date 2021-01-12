a34be705fc1ec9f56cfbdce32560157841ec39b0
Jerry Wang <moonspirit.wcy@gmail.com>
2021-01-12 22:22:59 +0800

    redux

---
+++ src/redux/modules/rolloutOperation.js
@@ -0,0 +1,26 @@
+export const EXECUTE_OPERATION = "EXECUTE_OPERATION";
+
+const initialState = {
+    operation: "",
+};
+
+export default function rolloutOperationReducer(
+    state = initialState,
+    action = {}
+) {
+    switch (action.type) {
+        case EXECUTE_OPERATION:
+          return {
+            ...state,
+            operation: action.operation,
+          };
+    
+        default:
+          return state;
+    }
+}
+
+export const executeOperation = (operation) => ({
+    type: EXECUTE_OPERATION,
+    operation
+});
